7799a01c8b56a39c8a2802ff442220a0d1a089f2
nacean <nacean@naver.com>
2022-04-24 16:45:45 +0900

    Delete unecessary files

---
+++ newflow-frontend/src/pages/MainContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function MainContainer() {
+  return (
+    <article className="mainContainer">
+      <header></header>
+      <main></main>
+      <footer></footer>
+    </article>
+  );
+}
+
+export default MainContainer;
