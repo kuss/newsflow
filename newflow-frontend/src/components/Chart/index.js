103f12597cabeacf42bde1d511b5065b449baad0
y00njae <rladbswo12@gist.ac.kr>
2022-04-24 17:43:36 +0900

    add gdp tooltip

--- newflow-frontend/src/components/Chart/index.js
+++ newflow-frontend/src/components/Chart/index.js
@@ -15,7 +15,21 @@ const Chart = () => {
 			type: 'value',
 			min: 1700000,
       max: 2200000,
-	  },
+		},
+		tooltip: {
+      trigger: 'axis',
+				axisPointer: {
+					type: 'shadow',
+				},
+			formatter: (params) => {
+				console.log(params)
+				return (
+					`$${params[0].data.toLocaleString()}` +
+					'<br/>' +
+					`${params[0].axisValue}년`
+				);
+			},
+		},
 	  series: [
 	    {
 	      data: [1835698.2, 1898192.6, 1924498.1, 1933152.4, 2057447.8],
